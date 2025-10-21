6c3c2a32bede072102f04b9b69d80e33e963bb8f
jerpen80 <dra_penny@yahoo.com>
2025-10-21 13:58:27 +0200

    fix issue

--- prowler_frontend/modules/lambda/main.tf
+++ prowler_frontend/modules/lambda/main.tf
@@ -1,20 +1,14 @@
-terraform {
-  required_version = ">= 1.0.2"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.9.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.2.0, < 3.0.0"
-    }
-  }
+locals {
+  base_path          = "${path.module}/../../external/cloudfront-authorization-at-edge/${var.function}"
+  build_dir          = "${path.root}/.terraform-build"
+  zip_output_path    = "${local.build_dir}/${var.function}-v1.zip"
+  path_configuration = "${local.base_path}/configuration.json"
 }
 
-locals {
-  base_path         = "${path.module}/../../external/cloudfront-authorization-at-edge/${var.function}"
-  path_configuration = "${local.base_path}/configuration.json"
+resource "null_resource" "prepare_build_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.build_dir}"
+  }
 }
 
 resource "local_file" "function_configuration" {
@@ -23,13 +17,14 @@ resource "local_file" "function_configuration" {
 }
 
 data "archive_file" "archive" {
-  type        = "zip"
-  source_dir  = local.base_path
-  output_path = "${local.base_path}-v1.zip"
-  output_file_mode = "0666"
+  type               = "zip"
+  source_dir         = local.base_path
+  output_path        = local.zip_output_path
+  output_file_mode   = "0666"
 
   depends_on = [
     local_file.function_configuration,
+    null_resource.prepare_build_dir,
   ]
 }
 
@@ -45,12 +40,12 @@ module "lambda_function" {
   lambda_at_edge = true
 
   create_package         = false
-  local_existing_package = abspath(data.archive_file.archive.output_path)
+  local_existing_package = abspath(local.zip_output_path)
 
   tracing_mode = "Active"
 
   depends_on = [
+    data.archive_file.archive,
     local_file.function_configuration,
-    data.archive_file.archive
   ]
 }
