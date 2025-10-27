d0a40fdfa00d42939cc8f8d34ce46fbaeb4cc091
jerpen80 <dra_penny@yahoo.com>
2025-10-27 13:43:20 +0100

    remove duplicate data block

--- main.tf
+++ main.tf
@@ -48,27 +48,3 @@ module "prowler_scan" {
   mutelist                     = var.mutelist
   depends_on = [aws_route53_zone.prowlersite]
 }
-
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
-
-data "aws_subnets" "all" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-}
-
-data "aws_subnet" "selected" {
-  for_each = toset(data.aws_subnets.all.ids)
-  id       = each.value
-}
-
-# filter only public subnets
-locals {
-  public_subnet_ids = [
-    for subnet_id, subnet in data.aws_subnet.selected : subnet_id
-    if subnet.map_public_ip_on_launch == true
-  ]
-}
